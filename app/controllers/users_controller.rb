ec89915ae8d48b8a8662a49b5780cad12b5e0d88
denisdiaconu <denisdiaconu117@yahoo.com>
2021-11-02 16:39:34 +0100

    Add seed for users,comments,posts,likes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,50 +1,9 @@
 class UsersController < ApplicationController
   def index
-    @users = [
-      {
-        name: 'Giuseppe',
-        photo: 'https://images-na.ssl-images-amazon.com/images/I/41vMBIeF-zL._SX384_BO1,204,203,200_.jpg',
-        bio: 'Did my opinion offend you? You should hear the ones I don’t say out loud.',
-        postsCounter: 2
-      },
-      {
-        name: 'Marco',
-        photo: 'https://images-na.ssl-images-amazon.com/images/I/41vMBIeF-zL._SX384_BO1,204,203,200_.jpg',
-        bio: 'A bus station is where a bus stops. A train station is where a train stops.',
-        postsCounter: 1
-      },
-      {
-        name: 'Simon',
-        photo: 'https://images-na.ssl-images-amazon.com/images/I/41vMBIeF-zL._SX384_BO1,204,203,200_.jpg',
-        bio: 'Born at a very young age.',
-        postsCounter: 2
-      }
-    ]
+    @users = User.all
   end
 
-  # rubocop:disable Metrics/MethodLength
   def show
-    @user =
-      {
-        name: 'Giuseppe',
-        photo: 'https://images-na.ssl-images-amazon.com/images/I/41vMBIeF-zL._SX384_BO1,204,203,200_.jpg',
-        bio: 'Did my opinion offend you? You should hear the ones I don’t say out loud.',
-        postsCounter: 2
-      }
-    @posts = [
-      {
-        title: 'post 1',
-        text: 'this is post content'
-      },
-      {
-        title: 'post 2',
-        text: 'this is post content'
-      },
-      {
-        title: 'post 3',
-        text: 'this is post content'
-      }
-    ]
+    @user = User.find(params[:id])
   end
-  # rubocop:enable Metrics/MethodLength
 end
