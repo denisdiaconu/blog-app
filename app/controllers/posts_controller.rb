8c604750a6188e7c84a943ce3bc1e3a6097ee0c2
denisdiaconu <denisdiaconu117@yahoo.com>
2021-11-02 14:54:32 +0100

    Add views for all routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,81 @@
 class PostsController < ApplicationController
+  # rubocop:disable Metrics/MethodLength
   def index
     @user_id = params[:user_id]
+    @user =
+      {
+        name: 'Giuseppe',
+        photo: 'https://images-na.ssl-images-amazon.com/images/I/41vMBIeF-zL._SX384_BO1,204,203,200_.jpg',
+        bio: 'Did my opinion offend you? You should hear the ones I don’t say out loud.',
+        postsCounter: 2
+      }
+    @posts = [
+      {
+        title: 'post 1',
+        text: 'this is post content',
+        comments: [
+          {
+            text: 'hello world'
+          },
+          {
+            text: 'hello world 2'
+          },
+          {
+            text: 'hello world 3'
+          }
+        ]
+      },
+      {
+        title: 'post 2',
+        text: 'this is post content',
+        comments: [
+          {
+            text: 'hello world'
+          },
+          {
+            text: 'hello world 2'
+          },
+          {
+            text: 'hello world 3'
+          }
+        ]
+      },
+      {
+        title: 'post 3',
+        text: 'this is post content',
+        comments: [
+          {
+            text: 'hello world'
+          },
+          {
+            text: 'hello world 2'
+          },
+          {
+            text: 'hello world 3'
+          }
+        ]
+      }
+    ]
   end
+  # rubocop:enable Metrics/MethodLength
 
   def show
     @id = params[:id]
     @user_id = params[:user_id]
+    @post = {
+      title: 'post 1',
+      text: 'this is post content',
+      comments: [
+        {
+          text: 'hello world'
+        },
+        {
+          text: 'hello world 2'
+        },
+        {
+          text: 'hello world 3'
+        }
+      ]
+    }
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,50 @@
 class UsersController < ApplicationController
-  def index; end
-
-  def show
-    @id = params[:id]
+  def index
+    @users = [
+      {
+        name: 'Giuseppe',
+        photo: 'https://images-na.ssl-images-amazon.com/images/I/41vMBIeF-zL._SX384_BO1,204,203,200_.jpg',
+        bio: 'Did my opinion offend you? You should hear the ones I don’t say out loud.',
+        postsCounter: 2
+      },
+      {
+        name: 'Marco',
+        photo: 'https://images-na.ssl-images-amazon.com/images/I/41vMBIeF-zL._SX384_BO1,204,203,200_.jpg',
+        bio: 'A bus station is where a bus stops. A train station is where a train stops.',
+        postsCounter: 1
+      },
+      {
+        name: 'Simon',
+        photo: 'https://images-na.ssl-images-amazon.com/images/I/41vMBIeF-zL._SX384_BO1,204,203,200_.jpg',
+        bio: 'Born at a very young age.',
+        postsCounter: 2
+      }
+    ]
   end
+
+  # rubocop:disable Metrics/MethodLength
+  def show
+    @user =
+      {
+        name: 'Giuseppe',
+        photo: 'https://images-na.ssl-images-amazon.com/images/I/41vMBIeF-zL._SX384_BO1,204,203,200_.jpg',
+        bio: 'Did my opinion offend you? You should hear the ones I don’t say out loud.',
+        postsCounter: 2
+      }
+    @posts = [
+      {
+        title: 'post 1',
+        text: 'this is post content'
+      },
+      {
+        title: 'post 2',
+        text: 'this is post content'
+      },
+      {
+        title: 'post 3',
+        text: 'this is post content'
+      }
+    ]
+  end
+  # rubocop:enable Metrics/MethodLength
 end
