d4b7dd90511c517c5572016cc1a1a691a5942a49
denisdiaconu <denisdiaconu117@yahoo.com>
2021-11-05 16:02:51 +0100

    Added validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,11 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
   validates :title, :text, presence: true
-
+  validates :commentsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likesCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, length: { maximum: 250,
+    too_long: "%{count} characters is the maximum allowed" }
+  
   def update_post_counter(id)
     user = User.find_by(id: id)
     user.postsCounter += 1
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :likes
 
   validates :name, :photo, :bio, :postsCounter, presence: true
+  validates :postsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def last_posts(id)
     User.find_by(id: id).posts.last(3)
