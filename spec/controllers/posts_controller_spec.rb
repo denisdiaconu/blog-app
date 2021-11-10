6204a3d56e7180d246dcab69abafdf9955bf347a
denisdiaconu <denisdiaconu117@yahoo.com>
2021-11-10 20:40:44 +0100

    Finally it-s working

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :request do
   include Devise::Test::IntegrationHelpers
-  let(:user) { User.create(name: 'Cork', email: 'example@mail.com', password: 'password', photo: 'www.google.com') }
+  let(:user) { User.create(name: 'Cork', email: 'example@mail.com', password: 'password') }
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :request do
   include Devise::Test::IntegrationHelpers
-  let(:user) { User.create(name: 'Cork', email: 'example@mail.com', password: 'password', photo: 'www.google.com') }
+  let(:user) { User.create(name: 'Cork', email: 'example@mail.com', password: 'password') }
 describe 'GET #index' do
     before do
       sign_in user
-      get users_path
+      get root_path
     end
     it 'should have response status correct(ok)' do
       expect(response).to have_http_status(:ok)
